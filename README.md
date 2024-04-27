40af1305b80a3f2ecb1359a2d3521e8d287d4239
ErnestasRaw <ernestasjasi@gmail.com>
2024-04-27 21:33:06 +0300

    Initial commit (file structure)

---
+++ README.md
@@ -0,0 +1,14 @@
+# news_app
+
+News app that obtains articles from the newsapi.org.
+
+## Getting Started
+
+## Acknowledgments
+
+This application utilizes the News API to retrieve news articles and information.
+
+### News API
+
+- Website: [News API](https://newsapi.org)
+- Terms of Service: [Terms of Service](https://newsapi.org/terms)
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'News App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeView(title: 'News App Home View'),
+    );
+  }
+}
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key, required this.title});
+  final String title;
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Initial'),
+          ],
+        ),
+      ),
+    );
+  }
+}
